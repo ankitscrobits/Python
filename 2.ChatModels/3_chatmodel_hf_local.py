5364e6474b35d97b14216f4de69286263fd22be4
ankitscrobits <ankit.shrivastava@scrobits.com>
2026-08-12 20:29:44 +0530

    Create 3_chatmodel_hf_local.py
    Tested langChain Chat model using a huggfing face model locally

---
+++ 2.ChatModels/3_chatmodel_hf_local.py
@@ -0,0 +1,18 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
+
+llm = HuggingFacePipeline.from_model_id(
+    model_id="Qwen/Qwen2.5-0.5B-Instruct",
+    task="text-generation",
+    pipeline_kwargs={
+        "max_new_tokens": 4,
+        "top_k": 30,
+        "temperature": 0.7,
+        "repetition_penalty": 1.25
+    }
+)
+
+model = ChatHuggingFace(llm=llm)
+
+result = model.invoke("What is the capital of India?")
+
+print(result.content)
